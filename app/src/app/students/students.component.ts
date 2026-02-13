0fd7fa0ace260f9758b1b4953b286147509d26a1
Richard Myers <richard.myers@mapof.ag>
2026-02-13 16:27:13 +0000

    Initial Codebase

---
+++ app/src/app/students/students.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+               selector:    'app-students',
+               templateUrl: './students.component.html',
+               standalone:  false,
+           })
+export class StudentsComponent {
+
+    constructor() {}
+
+}
+
